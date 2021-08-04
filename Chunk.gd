f3a5c514af070474e40a71030f8e752f3d7cda73
Edward Gorman <ejgorman@gmail.com>
2021-08-04 07:50:57 +0100

    Add chunk textures, remove inside block faces

--- Chunk.gd
+++ Chunk.gd
@@ -25,8 +25,34 @@ var st = SurfaceTool.new()
 var mesh = null
 var mesh_instance = null
 
+var material = preload("res://assets/new_spatialmaterial.tres")
+
 func _ready():
+	material.albedo_texture.set_flags(2)
+	
+	generate()
 	update()
+
+func generate():
+	blocks = []
+	blocks.resize(Global.DIMENSION.x)
+	for i in range(0, Global.DIMENSION.x):
+		blocks[i] = []
+		blocks[i].resize(Global.DIMENSION.y)
+		for j in range(0, Global.DIMENSION.y):
+			blocks[i][j] = []
+			blocks[i][j].resize(Global.DIMENSION.z)
+			for k in range(0, Global.DIMENSION.z):
+				var block = Global.AIR
+				
+				if j < 16:
+					block = Global.STONE
+				elif j < 32:
+					block = Global.DIRT
+				elif j == 32:
+					block = Global.GRASS
+				
+				blocks[i][j][k] = block
 
 func update():
 	# Unload mesh
@@ -47,27 +73,55 @@ func update():
 	
 	# Set mesh normals and material
 	st.generate_normals(false)
-	st.set_material(mesh)
+	st.set_material(material)
 	st.commit(mesh)
 	mesh_instance.set_mesh(mesh)
 	
 	add_child(mesh_instance)
 	mesh_instance.create_trimesh_collision()
 
-func create_block(x, y, z):
-	create_face(TOP, x, y, z)
-	create_face(BOTTOM, x, y, z)
-	create_face(LEFT, x, y, z)
-	create_face(RIGHT, x, y, z)
-	create_face(FRONT, x, y, z)
-	create_face(BACK, x, y, z)
+func check_transparent(x, y, z):
+	if x >= 0 and x < Global.DIMENSION.x and \
+		y >= 0 and y < Global.DIMENSION.y and \
+		z >= 0 and z < Global.DIMENSION.z:
+			return not Global.types[blocks[x][y][z]][Global.SOLID]
+	return true
 
-func create_face(i, x, y, z):
+func create_block(x, y, z):
+	var block = blocks[x][y][z]
+	if block == Global.AIR:
+		return
+	
+	var block_info = Global.types[block]
+	
+	if check_transparent(x, y + 1, z):
+		create_face(TOP, x, y, z, block_info[Global.TOP])
+	if check_transparent(x, y - 1, z):
+		create_face(BOTTOM, x, y, z, block_info[Global.BOTTOM])
+	if check_transparent(x - 1, y, z):
+		create_face(LEFT, x, y, z, block_info[Global.LEFT])
+	if check_transparent(x + 1, y, z):
+		create_face(RIGHT, x, y, z, block_info[Global.RIGHT])
+	if check_transparent(x, y, z - 1):
+		create_face(BACK, x, y, z, block_info[Global.BACK])
+	if check_transparent(x, y, z + 1):
+		create_face(FRONT, x, y, z, block_info[Global.FRONT])
+
+func create_face(i, x, y, z, texture_atlas_offset):
 	var offset = Vector3(x, y, z)
 	var a = vertices[i[0]] + offset
 	var b = vertices[i[1]] + offset
 	var c = vertices[i[2]] + offset
 	var d = vertices[i[3]] + offset
 	
-	st.add_triangle_fan(([a, b, c]))
-	st.add_triangle_fan(([a, c, d]))
+	var uv_offset = texture_atlas_offset / Global.TEXTURE_ATLAS_SIZE
+	var height = 1.0 / Global.TEXTURE_ATLAS_SIZE.y
+	var width = 1.0 / Global.TEXTURE_ATLAS_SIZE.x
+	
+	var uv_a = uv_offset + Vector2(0, 0)
+	var uv_b = uv_offset + Vector2(0, height)
+	var uv_c = uv_offset + Vector2(width, height)
+	var uv_d = uv_offset + Vector2(width, 0)
+	
+	st.add_triangle_fan(([a, b, c]), ([uv_a, uv_b, uv_c]))
+	st.add_triangle_fan(([a, c, d]), ([uv_a, uv_c, uv_d]))
